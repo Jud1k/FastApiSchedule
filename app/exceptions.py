855faf9512cb61348da7f6ccda779509f289c0f8
Jud1k <astronsahsa@mail.ru>
2025-11-14 23:23:58 +0500

    update NotFoundErr and ConflictErr

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,12 +1,21 @@
 from fastapi import HTTPException, status
 
 
-class NotFoundError(Exception):
-    pass
+class NotFoundErr(HTTPException):
+    def __init__(self, record_name="Record", record_id=None, headers=None):
+        """Custom HTTPException for 404 Not Found errors.
+
+        Provides consistent formatting for "resource not found" scenarios
+        with flexible messaging based on provided parameters."""
+        message = f"{record_name} not found" if record_id is None else f"{record_name} with ID {record_id} not found"
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=message, headers=headers)
 
 
-class ConflictError(Exception):
-    pass
+class ConflictErr(HTTPException):
+    def __init__(self, record_name="Record", headers=None):
+        """Custom HTTPException for 409 Conflict errors."""
+        message = f"{record_name} already exist"
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=message, headers=headers)
 
 
 PasswordsDoNotMatch = HTTPException(
