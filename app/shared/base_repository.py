462a690daf856bfa16bffd4ad24f16ed8382e26f
Jud1k <astronsahsa@mail.ru>
2025-11-14 23:22:08 +0500

    add logging

--- app/shared/base_repository.py
+++ app/shared/base_repository.py
@@ -16,46 +16,73 @@ T = TypeVar("T", bound=Base)
 
 
 class SqlAlchemyRepository(Generic[T]):
-    model: type[T] = None
+    model: type[T]
 
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_all(self) -> list[T]:
-        stmt = select(self.model)
-        res = await self.session.execute(stmt)
-        return list(res.scalars().all())
-
+    async def get_all(self,skip:int=0,limit:int=100) -> list[T]:
+        logger.info(f"Search all records {self.model.__name__}.")
+        stmt = select(self.model).offset(skip).limit(limit)
+        result = await self.session.execute(stmt)
+        records = list(result.scalars().all())
+        logger.info(f"Find {len(records)} records.")
+        return records
+    
     async def get_one_or_none_by_id(self, id: int) -> T | None:
-        stmt = select(self.model).filter(self.model.id == id)
-        res = await self.session.execute(stmt)
-        return res.scalar_one_or_none()
+        logger.info(f"Search {self.model.__name__} with ID {id}.")
+        stmt = select(self.model).filter(self.model.id == id) # type: ignore
+        result = await self.session.execute(stmt)
+        record = result.scalar_one_or_none()
+        if record:
+            logger.info(f"Record with ID {id} find.")
+        else:
+            logger.info(f"Record with ID {id} does not find.")
+        return record 
 
     async def get_one_or_none(self, filters: BaseModel) -> T | None:
         filters_dict = filters.model_dump(exclude_unset=True)
+        logger.info(f"Search {self.model.__name__} with filters: {filters_dict}.")
         stmt = select(self.model).filter_by(**filters_dict)
-        res = await self.session.execute(stmt)
-        return res.scalar_one_or_none()
-
-    async def create(self, data: dict) -> T:
-        value = self.model(**data)
+        result = await self.session.execute(stmt)
+        record = result.scalar_one_or_none()
+        if record:
+            logger.info(f"Record find by filters: {filters_dict}.")
+        if not record:
+            logger.info(f"Record not find by filters: {filters_dict}.")
+        return record
+            
+    async def create(self, data: BaseModel) -> T:
+        logger.info(f"Creating record {self.model.__name__} with data:{data}.")
+        data_dict = data.model_dump(exclude_unset=True)
+        value = self.model(**data_dict)
         self.session.add(value)
         await self.session.flush()
+        logger.info(f"Record {self.model.__name__} successfully created.")
         return value
 
-    async def create_many(self, data: list[dict]) -> list[T]:
-        values = [self.model(**d) for d in data]
+    async def create_many(self, data: list[BaseModel]) -> list[T]:
+        data_list = [record.model_dump(exclude_unset=True) for record in data]
+        logger.info(f"Creating records {self.model.__name__}. Count:{len(data_list)}")
+        values = [self.model(**item) for item in data_list]
         self.session.add_all(values)
         await self.session.flush()
+        logger.info(f"{len(data_list)} records successfully created.")
         return values
 
-    async def update(self, data: dict, update_data: dict) -> T:
-        for key, value in update_data.items():
-            setattr(data, key, value)
-            await self.session.flush()
-        return data # type: ignore
+    async def update(self, data: T, update_data: BaseModel) -> T:
+        data_dict = data.to_dict()
+        update_data_dict = update_data.model_dump(exclude_unset=True)
+        for field in data_dict:
+            if field in update_data_dict:
+                setattr(data,field,update_data_dict[field])
+        await self.session.flush()
+        logger.info(f"Record {self.model.__name__} successfully updated.")
+        return data
 
-    async def delete(self, id: int):
+    async def delete(self, id: int)->None:
+        logger.info(f"Deleting record with ID {id}.")
         stmt = delete(self.model).filter(self.model.id == id) # type: ignore
         await self.session.execute(stmt)
         await self.session.flush()
+        logger.info(f"Record with ID {id} successfully deleted.")
